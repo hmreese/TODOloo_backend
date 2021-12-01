2e181515fea77f8744805543b8d583489ed72d3e
hmreese <hmreese308@gmail.com>
2021-12-01 00:47:12 -0800

    changes

--- backend.py
+++ backend.py
@@ -163,7 +163,13 @@ def get_friends(username):
         except:
             return jsonify({}), 400
         ret = User().add_friend(username, fUsername)
-        return jsonify({'friend': ret}), 200
+        user = User().find_by_username(username)
+        lists = user[0]["friends"]
+        friendList = []
+        for i in lists:
+            fren = User().find_by_username(i)
+            friendList.append(fren)
+        return jsonify(friendList), 200
 
 
 @app.route('/', methods=['GET', 'POST'])
